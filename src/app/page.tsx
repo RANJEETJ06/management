75e0ef960652ef2885c5faffd4ee51089cddf7b9
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2026-05-11 16:30:45 +0530

    fix: update authentication method to use session instead of user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { createClient } from "@/lib/supabase/server";
 export default async function Home() {
   const supabase = createClient();
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  redirect(user ? "/dashboard" : "/login");
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  redirect(session ? "/dashboard" : "/login");
 }
